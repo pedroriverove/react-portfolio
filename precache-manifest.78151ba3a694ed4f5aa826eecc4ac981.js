6abbae87da93ae11fe982ed9d511fb2cbdeb0774
Pedro Rivero <pedro_rivero_321@hotmail.com>
2022-10-06 04:24:55 -0400

    Updates

---
+++ precache-manifest.78151ba3a694ed4f5aa826eecc4ac981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d271ea5f9c2b31ec8ebf13bf4af8cf6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f69b9acdb256048a36cb",
+    "url": "/static/css/2.d8077e82.chunk.css"
+  },
+  {
+    "revision": "d409b85e2d1ac77f8b45",
+    "url": "/static/css/main.62e4f1a6.chunk.css"
+  },
+  {
+    "revision": "f69b9acdb256048a36cb",
+    "url": "/static/js/2.29d69875.chunk.js"
+  },
+  {
+    "revision": "45dd6a8eb784f4bfc84b6e54cbce993a",
+    "url": "/static/js/2.29d69875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d409b85e2d1ac77f8b45",
+    "url": "/static/js/main.0c0da274.chunk.js"
+  },
+  {
+    "revision": "ecbb5a4aebff9c89b516",
+    "url": "/static/js/runtime-main.ff035f97.js"
+  }
+]);
