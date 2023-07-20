ca60b457bcebc970dad6349881411785c4f660f2
Pedro Rivero <pedro_rivero_321@hotmail.com>
2023-07-20 16:57:27 -0400

    feat: :zap: creando funcion renderSkill para optimizar codigo

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,99 +1,31 @@
 import React, {Component} from "react";
 
+function renderSkill(skills, i) {
+	return (
+		<li className="list-inline-item mx-3" key={i}>
+      <span>
+        <div className="text-center skills-tile">
+          <i className={skills.class} style={{fontSize: "220%"}}>
+            <p className="text-center" style={{fontSize: "30%", marginTop: "4px"}}>
+              {skills.name}
+            </p>
+          </i>
+        </div>
+      </span>
+		</li>
+	);
+}
+
 class Skills extends Component {
 	render() {
 		if (this.props.sharedSkills && this.props.resumeBasicInfo) {
 			var sectionName = this.props.resumeBasicInfo.section_name.skills;
-			var frontBasic = this.props.sharedSkills.icons.front.basic.map(function (skills, i) {
-				return (
-					<li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{fontSize: "220%"}}>
-                  <p
-										className="text-center"
-										style={{fontSize: "30%", marginTop: "4px"}}
-									>
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-					</li>
-				);
-			});
-			var frontAdvanced = this.props.sharedSkills.icons.front.advanced.map(function (skills, i) {
-				return (
-					<li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{fontSize: "220%"}}>
-                  <p
-										className="text-center"
-										style={{fontSize: "30%", marginTop: "4px"}}
-									>
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-					</li>
-				);
-			});
-			var backBasic = this.props.sharedSkills.icons.back.basic.map(function (skills, i) {
-				return (
-					<li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{fontSize: "220%"}}>
-                  <p
-										className="text-center"
-										style={{fontSize: "30%", marginTop: "4px"}}
-									>
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-					</li>
-				);
-			});
-			var backAdvanced = this.props.sharedSkills.icons.back.advanced.map(function (skills, i) {
-				return (
-					<li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{fontSize: "220%"}}>
-                  <p
-										className="text-center"
-										style={{fontSize: "30%", marginTop: "4px"}}
-									>
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-					</li>
-				);
-			});
-			var tools = this.props.sharedSkills.icons.tools.map(function (skills, i) {
-				return (
-					<li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{fontSize: "220%"}}>
-                  <p
-										className="text-center"
-										style={{fontSize: "30%", marginTop: "4px"}}
-									>
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-					</li>
-				);
-			});
+			var backend = this.props.sharedSkills.icons.backend.map(renderSkill);
+			var databases = this.props.sharedSkills.icons.databases.map(renderSkill);
+			var devops = this.props.sharedSkills.icons.devops.map(renderSkill);
+			var frontendBasic = this.props.sharedSkills.icons.frontend.basic.map(renderSkill);
+			var frontendAdvanced = this.props.sharedSkills.icons.frontend.advanced.map(renderSkill)
+			var git = this.props.sharedSkills.icons.git.map(renderSkill);
 		}
 
 		return (
@@ -106,23 +38,34 @@ class Skills extends Component {
 					</div>
 					<div className="col-md-12 text-center">
 						<h2>
-							<span className="text-white">Frontend</span>
+							<span className="text-white">Databases</span>
 						</h2>
-						<ul className="list-inline mx-auto skill-icon">{frontBasic}</ul>
-						<ul className="list-inline mx-auto skill-icon">{frontAdvanced}</ul>
+						<ul className="list-inline mx-auto skill-icon">{databases}</ul>
 					</div>
 					<div className="col-md-12 text-center pt-3">
 						<h2>
-							<span className="text-white">Backend</span>
+							<span className="text-white">Back-end</span>
 						</h2>
-						<ul className="list-inline mx-auto skill-icon">{backBasic}</ul>
-						<ul className="list-inline mx-auto skill-icon">{backAdvanced}</ul>
+						<ul className="list-inline mx-auto skill-icon">{backend}</ul>
 					</div>
 					<div className="col-md-12 text-center pt-3">
 						<h2>
-							<span className="text-white">Repository</span>
+							<span className="text-white">Front-end</span>
 						</h2>
-						<ul className="list-inline mx-auto skill-icon">{tools}</ul>
+						<ul className="list-inline mx-auto skill-icon">{frontendBasic}</ul>
+						<ul className="list-inline mx-auto skill-icon">{frontendAdvanced}</ul>
+					</div>
+					<div className="col-md-12 text-center pt-3">
+						<h2>
+							<span className="text-white">DevOps</span>
+						</h2>
+						<ul className="list-inline mx-auto skill-icon">{devops}</ul>
+					</div>
+					<div className="col-md-12 text-center pt-3">
+						<h2>
+							<span className="text-white">Version control</span>
+						</h2>
+						<ul className="list-inline mx-auto skill-icon">{git}</ul>
 					</div>
 				</div>
 			</section>
