400d9b6355482a165b0137a09308e55cc44c8b97
Pedro Rivero <pedro_rivero_321@hotmail.com>
2025-04-01 18:38:10 -0400

    Updates

---
+++ precache-manifest.da5bdea005fdc09dc9e638c0f0f349ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ce7b8cdd0943ecec453b2d2889ab22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61e6063b71faa1f0ee4f",
+    "url": "/static/css/2.d8077e82.chunk.css"
+  },
+  {
+    "revision": "f06deb623c4850ced827",
+    "url": "/static/css/main.417498a2.chunk.css"
+  },
+  {
+    "revision": "61e6063b71faa1f0ee4f",
+    "url": "/static/js/2.cac71303.chunk.js"
+  },
+  {
+    "revision": "334a3166bc9976dda67650e8c6f88652",
+    "url": "/static/js/2.cac71303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06deb623c4850ced827",
+    "url": "/static/js/main.f39b4006.chunk.js"
+  },
+  {
+    "revision": "ecbb5a4aebff9c89b516",
+    "url": "/static/js/runtime-main.ff035f97.js"
+  }
+]);
